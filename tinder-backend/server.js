ac32bae05d317dd69d2a3233ebe5554e313b8083
Sahail-N-Raj <sahiraj98@gmail.com>
2022-02-08 21:07:27 +0530

    updated

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -22,19 +22,6 @@ mongoose.connect(connection_url, (err, info) => {
 app.get("/", (req, res) => {
   res.status(200).send("Hello sahail");
 });
-
-app.post("/tinder/cards ", (req, res) => {
-  const dbCard = req.body;
-  console.log("hello");
-
-  Cards.create(dbCard, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(201).send(data);
-    }
-  });
-});
 //to download data from database
 app.get("/tinder/cards", (req, res) => {
   Cards.find((err, data) => {
@@ -45,5 +32,16 @@ app.get("/tinder/cards", (req, res) => {
     }
   });
 });
+app.post("/tinder/cards", (req, res) => {
+  const dbCard = req.body;
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
 //listener
 app.listen(port, () => console.log(`listening on localhost:${port}`));
--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
+import axios from "./axios";
 
 function TinderCards() {
-  const [people, setPeople] = useState([
-    {
-      name: "Dhoni",
-      url: "https://c.ndtvimg.com/2021-10/gjo8hnng_ms-dhoni-ipl_625x300_07_October_21.jpg?output-quality=80&downsize=1278:*",
-    },
-    {
-      name: "Cristiano Ronaldo",
-      url: "https://i.dailymail.co.uk/1s/2022/01/10/11/52751219-10386457-image-a-1_1641815609197.jpg",
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/tinder/cards");
+      setPeople(req.data);
+    }
+    fetchData();
+  }, []);
+
   const swiped = (direction, nameToDelete) => {
     console.log("removing:" + nameToDelete);
   };
@@ -31,7 +32,7 @@ function TinderCards() {
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
             <div
-              style={{ backgroundImage: `url(${person.url})` }}
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
               className="card"
             >
               <h3>{person.name}</h3>
